ee88f2446be15d641721efc5ea90434477f7d646
fernandocieri <fernandocieri@gmail.com>
2022-04-18 20:25:43 +0200

    Creates and styles Introduction component

--- src/components/introduction/introduction.jsx
+++ src/components/introduction/introduction.jsx
@@ -2,6 +2,13 @@ import MinimizerTool from "../minimizerTool/minimizerTool"
 
 export default function Introduction() {
     return (
-        <MinimizerTool />
+        <section className='first-fold'>
+            <section className='introduction-wrapper'>
+                <h1>Minimize: an URL shortener</h1>
+                <p className='introduction'>Minimize™ is a free tool to shorten an URL or reduce a link. Use our Minimizer Tool™ to create a shortened link making it easy to remember and market.</p>
+                <p className='introduction'>Boost results. On top of better deliverability and click-through, rich link-level data gives you crucial insight into your link engagement so your team can make smarter decisions around its content and communications.</p>
+            </section>
+            <MinimizerTool />
+        </section>
     )
 }
