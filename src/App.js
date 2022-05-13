dd275ea2076d513a64c358488bf01515f59c7c5d
fernandocieri <fernandocieri@gmail.com>
2022-05-13 20:48:14 +0200

    Installs and uses react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
+import './assets/styles/compiledStyles.scss';
+import Redirection from "./components/redirection/redirection";
 import MainView from "./components/mainView/mainView";
-import './assets/styles/compiledStyles.scss'
+import NotFound from "./components/notFound/notFound";
 
 function App() {
   return (
-    <MainView />
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<MainView />} />
+        <Route path="/:customName/:shortUrl" element={<Redirection />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
